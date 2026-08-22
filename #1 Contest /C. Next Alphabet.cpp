32ca3d38c3e60d6a34afff00e35b6d69b0bc2401
Hamza Ahmed <250721788+hamza-ahmed26@users.noreply.github.com>
2026-08-22 12:32:46 +0300

    Add C. Next Alphabet solution

---
+++ #1 Contest /C. Next Alphabet.cpp
@@ -0,0 +1,16 @@
+// لينك السؤال : https://codeforces.com/group/MWSDmqGsZm/contest/326175/problem/C
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+    char C;
+    cin >> C;
+    if (C == 'z')
+    {
+        cout << "a";
+    }
+    else
+    {
+        cout << char(C + 1);
+    }
+    return 0;
+}
